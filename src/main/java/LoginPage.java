308cd74187cffd899f39be6ab3d6fdb1d744f702
Russ <qatsby@yandex.com>
2023-09-21 21:43:45 -0400

    POM created

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+// How to create Page Object Model (POM)
+
+// 1. Create a login page class
+public class LoginPage {
+
+    // 2. Add a private field Driver to the class
+    private WebDriver driver;
+
+    // 3. Create Locators
+    private By emailField = By.xpath("//input[@id = 'email']");
+    private By passwordField = By.xpath("//input[@id = 'password']");
+    private By signInButton = By.xpath("//button[@class = 'auth-form__button']");
+    private By singUpButton = By.xpath("//a[@class = 'header__auth-link']");
+
+
+    // 4. Create Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // 5. Create methods for locators
+    public boolean checkSignInIsEnabled() {
+        return driver.findElement(signInButton).isEnabled();
+    }
+
+    public void clickSignInButton() {
+        driver.findElement(signInButton).click();
+    }
+
+    public void clickSignUpButton() {
+        driver.findElement(singUpButton).click();
+    }
+
+    // метод проверяет текст кнопки «Регистрация»
+//        public void checkTextRegistrationButton() {
+//            String exectedTextButton = "Регистрация";
+//            assertEquals("Errors", exectedTextButton , driver.findElement (registrationButton).getText());
+//        }
+
+}
