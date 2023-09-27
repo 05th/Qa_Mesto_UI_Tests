e2163ba0cfe8ac1810709b322c01a243ec4493e0
Russ <qatsby@yandex.com>
2023-09-26 22:45:32 -0400

    new updates

--- src/main/java/ProfileEditPage.java
+++ src/main/java/ProfileEditPage.java
@@ -1,17 +1,23 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.junit.*;
 
 public class ProfileEditPage {
 
     private WebDriver driver;
 
-    private By userNameEdit = By.xpath("//input[@id = 'owner-name']");
-    private By userDescriptionEdit = By.xpath("//input[@id = 'owner-description']");
-    private By buttonProfileChangeSave = By.xpath("//button[@class = 'button popup__button']");
+    private By userName = By.xpath("//input[@id = 'owner-name']");
+    private By userDescription = By.xpath("//input[@id = 'owner-description']");
+    private By buttonSave = By.xpath("//button[@class = 'button popup__button']");
 
     public ProfileEditPage(WebDriver driver){
         this.driver = driver;
     }
 
+    public void EnterNewValue(String description) {
+        Assert.assertTrue(driver.findElement(userDescription)).isEnabled();
+        driver.findElement(userDescription).clear();
+        driver.findElement(userDescription).sendKeys(description);
+    }
 
 }
