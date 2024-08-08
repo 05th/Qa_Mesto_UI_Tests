ff433c32cb5a373f85ab828128542ded1d6392b4
05 <qatsby@yandex.com>
2024-08-07 21:34:17 -0400

    new class added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,25 @@
 package tests;
 
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
 }
