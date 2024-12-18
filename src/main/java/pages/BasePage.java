4e01ee052f676c582f9a49b2e72c6e288653742e
05 <qatsby@yandex.com>
2024-12-18 00:14:58 -0500

    .

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,51 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import java.time.Duration;
+/**
+ * Базовый класс для всех страниц, содержит общие методы.
+ */
+
 public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    // Конструктор базового класса
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        // Инициализация явных ожиданий
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    // Метод для открытия URL
+    public void open(String url) {
+        driver.get(url);
+    }
+
+    // Метод для ожидания видимости элемента
+    protected void waitForVisibility(By locator) {
+        wait.until(driver -> driver.findElement(locator).isDisplayed());
+    }
+
+    // Метод для клика по элементу
+    protected void click(By locator) {
+        waitForVisibility(locator);
+        driver.findElement(locator).click();
+    }
+
+    // Метод для ввода текста в элемент
+    protected void type(By locator, String text) {
+        waitForVisibility(locator);
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    // Метод для получения текста элемента
+    protected String getText(By locator) {
+        waitForVisibility(locator);
+        return driver.findElement(locator).getText();
+    }
+
 }
